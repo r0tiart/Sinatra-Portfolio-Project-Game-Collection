50ffc12686913af0f8515cfe9aca1bfaf4b341a4
r0tiart <antley.chu@gmail.com>
2017-06-02 16:01:46 -0400

    added bootstrap navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 	configure do
-    set :public_folder, 'public'
+    # set :public_folder, 'public'
     set :views, 'app/views'
 
     enable :sessions
