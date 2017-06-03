5f318990720350740df05483c90a35426db97369
r0tiart <antley.chu@gmail.com>
2017-06-03 18:06:56 -0400

    added a login form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -27,4 +27,22 @@ class UserController < ApplicationController
 			redirect to "/"
 		end
 	end
+
+	get "/login" do 
+		erb :"users/login"
+	end
+
+	post "/login" do 
+
+		user = User.find_by(username: params[:username])
+
+
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect to "/user/#{user.slug}"
+		else
+			session[:valid?] = false
+			redirect to "/login"
+		end
+	end
 end
