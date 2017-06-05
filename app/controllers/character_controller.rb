b8f07caedf2b6ab08bf898b67515004ca641e789
r0tiart <antley.chu@gmail.com>
2017-06-04 20:43:56 -0400

    created a view for characters - for specific games

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,9 +1,12 @@
 class CharacterController < ApplicationController
 
-	get "/characters" do
-		if logged_in?
+	get "/users/:slug/characters" do
+ 		if logged_in?
+ 			@logged_in = logged_in?
+			@game = Game.find_by_slug(params[:slug])
 			@user = current_user
-			@characters = @user.characters
+			@characters = @user.characters.select { |character| character.game_id == @game.id}
+
 			erb :"/characters/characters"
 		else
 			redirect to "/"	
